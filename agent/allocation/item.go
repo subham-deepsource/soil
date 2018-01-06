93761cb57471f71328b41fca8506cc2ed6750163
Alexander Dorofeev <aka.spin@gmail.com>
2018-01-06 05:33:46 +0100

    introduce Blob marshal, up deps

--- agent/allocation/item.go
+++ agent/allocation/item.go
@@ -8,13 +8,13 @@ import (
 )
 
 // Slice of recoverable entities
-type RecoverableSlice interface {
-	Append(v ItemUnmarshaller)
+type ItemSlice interface {
+	AppendItem(v ItemUnmarshaller)
 }
 
 // ItemUnmarshaller entity can recover own state
 type ItemUnmarshaller interface {
-	UnmarshalLine(line string) (err error)
+	UnmarshalItem(line string) (err error)
 }
 
 // Marshal item to pod manifest
@@ -22,19 +22,19 @@ type ItemMarshaller interface {
 	MarshalLine(w io.Writer) (err error)
 }
 
-// Recover items from pod unit header
-func Recover(v RecoverableSlice, empty ItemUnmarshaller, source string, prefixes []string) (err error) {
+// UnmershalItemSlice items from pod unit header
+func UnmershalItemSlice(v ItemSlice, empty ItemUnmarshaller, source string, prefixes []string) (err error) {
 	err = &multierror.Error{}
 	for _, line := range strings.Split(source, "\n") {
 		for _, prefix := range prefixes {
 			if strings.HasPrefix(line, prefix) {
 				cp, _ := copystructure.Copy(empty)
 				v1 := cp.(ItemUnmarshaller)
-				if rErr := v1.UnmarshalLine(line); rErr != nil {
+				if rErr := v1.UnmarshalItem(line); rErr != nil {
 					err = multierror.Append(err, rErr)
 					continue
 				}
-				v.Append(v1)
+				v.AppendItem(v1)
 			}
 		}
 	}
